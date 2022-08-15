2d6e4961d8d6b6f48689e705f2a8b62c062596f4
Van Trung <fe.trungvan@gmail.com>
2022-08-15 08:06:41 +0700

    4. Thiet ke Home page phan 2

--- src/components/RSlick/MultipleRowSlick.js
+++ src/components/RSlick/MultipleRowSlick.js
@@ -1,11 +1,18 @@
+/* eslint-disable react/jsx-pascal-case */
 import Slider from 'react-slick';
 // import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 // reactSlick css
 import 'slick-carousel/slick/slick-theme.css';
 
-import styleSlick from './MultipleRowSlick.module.css'; // Tang do uu tien css
-import Film from '../Film/Film';
+import styleSlick from './MultipleRowSlick.module.css'; // Tang do uu tien css.
+// import Film from '../Film/Film';
+import Film_Flip from '../Film/Film_Flip';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  SET_FILM_DANG_CHIEU,
+  SET_FILM_SAP_CHIEU,
+} from '../../redux/actions/types/QuanLyPhimType';
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -30,24 +37,33 @@ function SamplePrevArrow(props) {
 }
 
 function MultipleRowSlick({ arrFilm }) {
+  const dispatch = useDispatch();
+  const { dangChieu, sapChieu } = useSelector(
+    (state) => state.QuanLyPhimReducer
+  );
+
   console.log('arrFilm: ', arrFilm);
   const renderFilms = () => {
-    return arrFilm.map((item, index) => {
+    return arrFilm.slice(0, 12).map((item, index) => {
       return (
-        <div className={`${styleSlick['width-item']}`} key={index}>
-          <Film />
+        <div className="mt-2" key={index}>
+          <Film_Flip item={item} />
         </div>
       );
     });
   };
+  let activeClassDC = dangChieu === true ? 'active_Film' : 'none_active_Film';
+  let activeClassSC = sapChieu === true ? 'active_Film' : 'none_active_Film';
+  console.log('activeSC: ', activeClassSC);
+
   const settings = {
     className: 'center',
     centerMode: true,
     infinite: true,
     centerPadding: '60px',
-    slidesToShow: 3,
+    slidesToShow: 2,
     speed: 500,
-    rows: 1,
+    rows: 2,
     slidesPerRow: 2,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
@@ -55,19 +71,25 @@ function MultipleRowSlick({ arrFilm }) {
 
   return (
     <>
-      <h2>Multiple Rows</h2>
-      <Slider {...settings}>
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-        {renderFilms()}
-      </Slider>
+      <button
+        className={`${styleSlick[activeClassDC]} px-8 py-3 font-semibold rounded bg-gray-800 text-white mr-2`}
+        onClick={() => {
+          const action = { type: SET_FILM_DANG_CHIEU };
+          dispatch(action);
+        }}
+      >
+        PHIM ĐANG CHIẾU
+      </button>
+      <button
+        className={`${styleSlick[activeClassSC]} px-8 py-3 font-semibold rounded bg-white text-gray-800 border-gray-800 border`}
+        onClick={() => {
+          const action = { type: SET_FILM_SAP_CHIEU };
+          dispatch(action);
+        }}
+      >
+        PHIM SẮP CHIẾU
+      </button>
+      <Slider {...settings}>{renderFilms()}</Slider>
     </>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,25 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useEffect } from 'react';
 import HomeMenu from './HomeMenu/HomeMenu';
+import { layDanhSachPhimAction } from '../../redux/actions/QuanLyPhimActions';
 
 // Kêt nối redux
 import { useSelector, useDispatch } from 'react-redux';
-import Film from '../../components/Film/Film';
+// import Film from '../../components/Film/Film';
 import MultipleRowSlick from '../../components/RSlick/MultipleRowSlick';
 
 function Home(props) {
   console.log('Home: ', props);
   const { arrFilm } = useSelector((state) => state.QuanLyPhimReducer);
   console.log('arrFilm: ', arrFilm);
+  const dispatch = useDispatch();
+  console.log('propsHome: ', arrFilm);
+
+  useEffect(() => {
+    const action = layDanhSachPhimAction();
+    dispatch(action); //dispatch function từ thunk
+  }, []);
 
   // const renderFilms = () => {
   //     return arrFilm.map((phim, index) => {
